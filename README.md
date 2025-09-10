8cc786fe8378a396a11371626080e1afdd504552
AxiomWolf <geronimo.xl13@gmail.com>
2025-09-10 19:28:20 +0200

    Small Cleanup and README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Btop to Resonite Websocket
+
+> [!WARNING]  
+> This script is more of a proof of concept than anything else and needs to be improved.
+
+This script will automatically send the output of btop to all connected websocket clients using [Resonite](https://resonite.com/)'s text markup.
+
+## Dependencies
+
+- [Node.js](https://nodejs.org/en)
+- [btop](https://github.com/aristocratos/btop)
+
+## Usage
+
+Running with `node index.js` will start a websocket on port 8080 that will periodically `send the output of a btop session to all connected clients.
+
+In order to properly display in Resonite, it is recommended to set graph symbol to Block.
+
+A Resonite frontend can be found at `resrec:///U-1TtGn3kT3bc/R-2A131A9A2F1BD46D5723DDDC5B7E7B2B90D525C7B3D5C58D7282C14C193568F5`. It will automatically attempt to connect to the websocket as it is loaded.
--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import xterm from '@xterm/xterm'
 const { Terminal } = xterm;
 import { SerializeAddon } from "@xterm/addon-serialize";
 import { spawn } from 'node-pty'
-import fs from 'fs'
-import stripAnsi from "strip-ansi"
 import { WebSocketServer } from 'ws';
 
+// Setup websocket
 const wss = new WebSocketServer({ port: 8080 });
-
 wss.on('connection', function connection(ws)
 {
     ws.on('error', console.error);
@@ -19,9 +17,10 @@ wss.on('connection', function connection(ws)
 
 });
 
+// Create xterm.js instance
 const COLS = 120;
 const ROWS = 30;
-const term = new Terminal({ cols: COLS, rows: ROWS, scrollback: 0 }); // Create xterm.js instance
+const term = new Terminal({ cols: COLS, rows: ROWS, scrollback: 0 }); 
 const serializeAddon = new SerializeAddon();
 term.loadAddon(serializeAddon)
 const btopProcess = spawn("btop", ["-u", "1000"], {
@@ -36,8 +35,6 @@ const btopProcess = spawn("btop", ["-u", "1000"], {
 btopProcess.on("data", (data) =>
 {
     term.write(data); // Pipe process output to xterm.js
-    // console.clear();
-    // console.log(getVirtualBuffer(term)); // Print the virtual screen
 
     let serializedData = serializeAddon.serializeAsHTML({ scrollback: 0 })
 
@@ -48,53 +45,20 @@ btopProcess.on("data", (data) =>
     result = result.replaceAll("<div>", "")
     result = result.replaceAll("</div>", "\n")
 
+    // Convert color codes
     result = result.replaceAll(/<span style='color: (#[0-9a-fA-F]{6}); background-color: (#[0-9a-fA-F]{6});(?: font-weight: (\w+);)?'>/g, (match, color, background, weight) => `<color ${color}><mark ${background}>${weight ? "<b>" : ""}`)
     result = result.replaceAll(/<span style='background-color: (#[0-9a-fA-F]{6});'>/g, (match, background) => `<mark ${background}>`)
     result = result.replaceAll("</span>", "<i></closeall>")
     result = result.replaceAll("<span>", "")
 
-    // Color codes
-    // result = serializedData.replaceAll(/\x1b\[38;2;(\d{1,3});(\d{1,3});(\d{1,3})m/g, (match, r, g, b) => `<color #${parseInt(r, 10).toString(16)}${parseInt(g, 10).toString(16)}${parseInt(b, 10).toString(16)}>`)
-    // result = result.replaceAll(/\x1b\[48;2;(\d{1,3});(\d{1,3});(\d{1,3})m/g, (match, r, g, b) => `<mark #${parseInt(r, 10).toString(16)}${parseInt(g, 10).toString(16)}${parseInt(b, 10).toString(16)}>`)
-    // result = result.replaceAll(/\x1b\[0m/g, "</closeall>")
-    // result = result.replaceAll(/\x1b\[22m/g, "</closeall>")
-    // result = result.replaceAll(/\x1b\[1m/g, "<b>")
-    // result = result.replaceAll(/\x1b\[3m/g, "<i>")
-
-    // Cursor
-    // result = result.replaceAll(/\x1b\[(\d+);(\d+)f/g, (match, n, m) => `\x1e\x1d${n};${m};`)
-    // result = result.replaceAll(/\x1b\[(\d+)([A-D])/g, (match, n, direction) => `\x1e\x1d${n}${direction};`)
-
-    // Remove private characters
-    // result = result.replaceAll(/\x1b\[\?\d+\w/g, "")
-    // result = result.replaceAll(/X\x1e\x1d\d+C;/g, "\n")
-    // result = stripAnsi(result)
-
-    // fs.writeFileSync("./stdout.log", result)
-
+    // Send the string to each client
     wss.clients.forEach(client =>
     {
         if (client.readyState == WebSocket.OPEN)
         {
-            // client.send(getVirtualBuffer(term).join("\n"))
             client.send(result)
         }
     })
 
-    console.log("Write")
+    console.log("Data sent.")
 });
-
-// Function to extract the screen buffer
-function getVirtualBuffer(term)
-{
-    let buffer = term.buffer.active; // Get active buffer
-    let output = [];
-
-    for (let i = 0; i < term.rows; i++)
-    {
-        let line = buffer.getLine(i);
-        output.push(line ? `line.translateToString()` : ' '.repeat(COLS));
-    }
-
-    return output; // Returns an array of strings representing rows
-}
